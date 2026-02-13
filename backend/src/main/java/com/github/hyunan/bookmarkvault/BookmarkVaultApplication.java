e1e5fa1d0d64a743cd2d0e7e0af65abbf8a14074
Ray An <hsrayan05@gmail.com>
2026-02-13 09:33:09 -0800

    initial commit

---
+++ backend/src/main/java/com/github/hyunan/bookmarkvault/BookmarkVaultApplication.java
@@ -0,0 +1,13 @@
+package com.github.hyunan.bookmarkvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarkVaultApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/github/hyunan/bookmarkvault/BookmarkVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hyunan.bookmarkvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
